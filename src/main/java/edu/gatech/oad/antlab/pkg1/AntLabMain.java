e2e58eb4f0892a82e76f9f40401a1d720498f968
TiantongLi <35302852+TiantongLi@users.noreply.github.com>
2018-09-07 13:37:40 -0400

    Update AntLabMain.java

--- src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -66,7 +66,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
 		  Person3 p3 = new Person3("Tiantong Li");
-		  toPrint += p3.toString("TiantongLi");
+		  toPrint += p3.toString("tli385");
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("XiaoyingYang");
